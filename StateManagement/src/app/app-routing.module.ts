5504c918da41a818078cee53b4eb6dabe251c716
Scott Neu <scott.neu@infoworks-tn.com>
2021-07-01 09:01:33 -0500

    code port to ng12

--- StateManagement/src/app/app-routing.module.ts
+++ StateManagement/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoutableParentComponent } from './components/routable-parent/routable-parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RoutableParentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- StateManagement/src/app/app.module.ts
+++ StateManagement/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterActionComponent } from './components/routable-parent/left-side/counter-action/counter-action.component';
+import { LeftSideComponent } from './components/routable-parent/left-side/left-side.component';
+import { CounterResultComponent } from './components/routable-parent/right-side/counter-result/counter-result.component';
+import { RightSideComponent } from './components/routable-parent/right-side/right-side.component';
+import { RoutableParentComponent } from './components/routable-parent/routable-parent.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    RoutableParentComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    CounterActionComponent,
+    CounterResultComponent,
   ],
   imports: [
     BrowserModule,
